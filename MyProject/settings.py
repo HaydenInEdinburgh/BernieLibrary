b43ba781b4454baa5cb247fa4a87ff9e2ca615b9
Bernie <bernie@autoreg-206277.dyn.wpi.edu>
2020-02-17 14:25:45 -0500

    Update ALLOWED_HOSTS with site and development server URL

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'o5xkemg@s&aelzjwei&c*6gf&+_o+f
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['bernielib.herokuapp.com',]
+ALLOWED_HOSTS = ['bernielib.herokuapp.com','127.0.0.1',]
 
 
 # Application definition
